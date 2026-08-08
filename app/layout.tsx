71441d01dbd87cc744410d3056bddca38b29773e
bipolarizeinc <bipolarize1212@gmail.com>
2026-08-08 14:59:40 -0600

    Enable Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AmbientAudio } from "./components/AmbientAudio";
 import { PolarExperience } from "./components/PolarExperience";
 import { PolarIntroAutoRelease } from "./components/PolarIntroAutoRelease";
@@ -93,6 +94,7 @@ export default function RootLayout({
         {children}
         <AmbientAudio />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
